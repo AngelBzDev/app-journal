7308f4ae6d8ca0d68128893faaecca4913341e9c
AngelBzDev <angelbaez.dev@gmail.com>
2022-02-21 23:54:12 -0600

    Boton de regreso añadido

--- src/components/buttons/ButtonRound.js
+++ src/components/buttons/ButtonRound.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const ButtonRound = ({type, children, nameClass, moreClass}) => {
+const ButtonRound = ({type, children, nameClass, moreClass, action}) => {
+
+  const navigate = useNavigate()
+
   return (
-    <button type={type} className={`btn-circle ${nameClass} ${moreClass}`} >
+    <button type={type} className={`btn-circle ${nameClass} ${moreClass}`} onClick={() => action === 'return' && navigate(-1)} >
       {children}
     </button>
   )
