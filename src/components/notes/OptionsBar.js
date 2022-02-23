6378b59d318a9d0c3ed91a5d822f5a3286548b5f
AngelBzDev <angelbaez.dev@gmail.com>
2022-02-23 00:16:58 -0600

    refactorizacion de componente para el boton

--- src/components/notes/OptionsBar.js
+++ src/components/notes/OptionsBar.js
@@ -3,12 +3,16 @@ import ButtonRound from '../buttons/ButtonRound'
 
 import {BsArrowLeft} from 'react-icons/bs'
 import {BsImage} from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
 
 const OptionsBar = () => {
+
+  const navigate = useNavigate()
+
   return (
     <div className='container'>
       <div className='optionBar__container'>
-        <ButtonRound moreClass='mr-2' action={'return'}>
+        <ButtonRound moreClass='mr-2' onClick={() => navigate(-1)}>
           <BsArrowLeft size={'2rem'}/>
         </ButtonRound>
         <ButtonRound moreClass='mr-2'>
