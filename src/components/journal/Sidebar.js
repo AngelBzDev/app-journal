8f157f7685a77e71386624f1b39c7f9a1d9ac4c4
AngelBzDev <angelbaez.dev@gmail.com>
2022-02-23 00:15:29 -0600

    Habilitando nombre de usuario

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { startLogout } from "../../actions/auth";
 
 const Sidebar = () => {
   const dispatch = useDispatch();
+  const {name} = useSelector(store => store.auth )
+  const [firstName] = name !== null ? name.split(' ') : ''
 
   const handleLogout = () => {
     dispatch(startLogout());
@@ -12,7 +14,7 @@ const Sidebar = () => {
   return (
     <div className="journal__sidebar container">
       <div className="journal__sidebar--options">
-        <p className="journal__user">Angel</p>
+        <p className="journal__user">{firstName}</p>
         <button className="button btn-sign-out" onClick={handleLogout}>
           Cerrar Sesión
         </button>
