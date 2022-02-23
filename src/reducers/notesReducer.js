be2b76c17713f028b5cfd9d555816aeafa4d88fb
AngelBzDev <angelbaez.dev@gmail.com>
2022-02-23 00:37:25 -0600

    reducer de las notas

---
+++ src/reducers/notesReducer.js
@@ -0,0 +1,15 @@
+
+const initialState = {
+  notes: []
+}
+
+const notesReducer = (state = initialState, action) => {
+  switch(action.payload){
+
+    default:
+      return state
+  }
+
+}
+
+export default notesReducer
